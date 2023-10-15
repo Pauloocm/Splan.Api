f4079863d613b143ef17949705f6250095bd4d52
Pauloocm <paulooo.cm@gmail.com>
2023-10-15 11:43:49 -0300

    Fixing conflict

--- Splan.Platform/Application/Employee/Dtos/EmployeeDto.cs
+++ Splan.Platform/Application/Employee/Dtos/EmployeeDto.cs
@@ -11,7 +11,7 @@ namespace Splan.Platform.Application.Employee.Dtos
 
         public string EducationalBackground { get; set; }
 
-        public new ContractingRegime ContractRegime { get; set; }
+        public new HiringRegime ContractRegime { get; set; }
 
         public bool Coordinator { get; set; } = false;
 
@@ -28,7 +28,7 @@ namespace Splan.Platform.Application.Employee.Dtos
                 Name = name,
                 Position = position,
                 EducationalBackground = educationBackground,
-                ContractRegime = (ContractingRegime)Enum.Parse(typeof(ContractingRegime), ParseEnum.ParseIntToEnum(contractingRegimeId)),
+                ContractRegime = (HiringRegime)Enum.Parse(typeof(HiringRegime), ParseEnum.ParseIntToEnum(contractingRegimeId)),
                 Coordinator = coordinator,
                 RhClassification = rhClassification
             };
@@ -47,7 +47,7 @@ namespace Splan.Platform.Application.Employee.Dtos
                 Name = employee.Name,
                 Position = employee.Position,
                 EducationalBackground = employee.EducationalBackground,
-                ContractRegime = (ContractingRegime)Enum.Parse(typeof(ContractingRegime), ParseEnum.ParseIntToEnum(employee.ContractRegimeId)),
+                ContractRegime = (HiringRegime)Enum.Parse(typeof(HiringRegime), ParseEnum.ParseIntToEnum(employee.ContractRegimeId)),
                 Coordinator = employee.Coordinator,
                 RhClassification = employee.RhClassification
             };
